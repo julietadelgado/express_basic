f04f534ad4c0266379e9cfa43e45f0845c3d8557
Julieta Delgado <julidmeza@gmail.com>
2022-04-23 17:38:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // Using express object
 const express = require('express')
-const res = require('express/lib/response')
 
 // Express App
 const app = express()
@@ -8,7 +7,7 @@ const app = express()
 // Port in wich we are going to see our app: localhost:3000
 const port = 3000
 
-// Initial path, this path will respond to the url localhost:300
+// Initial path, this path will respond to the url localhost:3000
 app.get('/', (request, response) => {
     response.send('Hello world')
 })
