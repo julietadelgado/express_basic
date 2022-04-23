d71b54d37dd690a21ef3d44864ec1adbb9592b53
Julieta Delgado <julidmeza@gmail.com>
2022-04-22 19:48:23 -0500

    Create my first server with Express

---
+++ app.js
@@ -0,0 +1,19 @@
+// Using express object
+const express = require('express')
+const res = require('express/lib/response')
+
+// Express App
+const app = express()
+
+// Port in wich we are going to see our app: localhost:3000
+const port = 300
+
+// Initial path, this path will respond to the url localhost:300
+app.get('/', (request, response) => {
+    response.send('Hello world')
+})
+
+// Inititalize app
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
